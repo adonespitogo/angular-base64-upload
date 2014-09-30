47d0684d3b219ad3b861dfdc4d39418e471e6f35
Adones <pitogo.adones@gmail.com>
2014-09-30 20:36:51 +0800

    unignore dist

---
+++ dist/angular-base64-upload.js
@@ -0,0 +1,31 @@
+/*! angular-base64-upload - v0.0.1 - 2014-09-30
+* https://github.com/adonespitogo/angular-base64-upload
+* Copyright (c) Adones Pitogo <pitogo.adones@gmail.com> 2014; Licensed  */
+angular.module('naif.base64', [])
+.directive('baseSixtyFourInput', function () {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function (scope, elem, attrs, ngModel) {
+      var fileObject = {};
+      scope.readerOnload = function(e){
+        var base64 = btoa(e.target.result);
+        fileObject.base64 = base64;
+        scope.$apply(function(){
+          ngModel.$setViewValue(fileObject);
+        });
+      };
+
+      var reader = new FileReader();
+      reader.onload = scope.readerOnload;
+
+      elem.on('change', function() {
+        var file = elem[0].files[0];
+        fileObject.filetype = file.type;
+        fileObject.filename = file.name;
+        // converts file to binary string
+        reader.readAsBinaryString(file);
+      });
+    }
+  };
+});
