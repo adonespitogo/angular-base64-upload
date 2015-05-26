6f0733bf4a4b96986c554d23a2af04e2b13d6a55
Adones Pitogo <pitogo.adones@gmail.com>
2015-05-27 03:07:51 +0800

    added sample base64 obj

--- test/config/file_loader.js
+++ test/config/file_loader.js
@@ -8,6 +8,7 @@ module.exports = function (ANGULAR_VERSION) {
       "bower_components/angular-v"+ANGULAR_VERSION+"/angular.js",
       "bower_components/angular-mocks-v"+ANGULAR_VERSION+"/angular-mocks.js",
       "src/angular-base64-upload.js",
+      "test/helpers/_sample_base64_object.js",
       "test/helpers/globals.js",
       "test/helpers/mocks.js",
       "test/helpers/compiler.js",
